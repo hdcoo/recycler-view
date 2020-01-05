c32b3c846f989bda810b28bda8594bf0ba84bc75
HDC-326 <huangdc521@gmail.com>
2020-01-05 17:52:03 +0800

    feat: export LazyLoader

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Recycler from './Recycler';
 import Source from './Source';
 import Renderer from './Renderer';
+import LazyLoader from './LazyLoader';
 import { IRecycler, ISource, IRenderer } from './interfaces/recycler';
 
-export { Recycler, Source, Renderer, IRecycler, ISource, IRenderer };
+export { Recycler, Source, Renderer, IRecycler, ISource, IRenderer, LazyLoader };
